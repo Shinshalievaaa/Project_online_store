3b6aa67c6626005e000a58212f5a8e623ba3584f
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2026-05-13 21:47:41 +0500

    add product_detail.html

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, product_detail, contacts
 
 app_name = CatalogConfig.name
 
 
 urlpatterns = [
     path('', home, name='home'),
+    path('product_detail/<int:product_id>/', product_detail, name='product_detail'),
     path('contacts/', contacts, name='contacts'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -7,6 +7,13 @@ def home(request):
     context = {'products': products}
     return render(request, 'home.html', context)
 
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context)
+
+
 def contacts(request):
     if request.method == 'POST':
         name = request.POST['name']
