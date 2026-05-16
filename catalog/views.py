4cfa4b0320540a4ec68bda7663828ff367756ac6
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2026-05-16 18:44:42 +0500

    add new page - add product

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from catalog.models import Product
+from catalog.models import Product, Category
 
 def home(request):
     products = Product.objects.all()
@@ -12,6 +12,27 @@ def product_detail(request, product_id):
     product = Product.objects.get(id=product_id)
     context = {'product': product}
     return render(request, 'product_detail.html', context)
+
+
+def new_product(request):
+    categories = Category.objects.all()
+    if request.method == 'POST':
+        name = request.POST['name']
+        description = request.POST['description']
+        category_fk = request.POST['category_id']  # Assuming 'category' is the name for the select input
+        print(f'category_fk = {category_fk}')
+        category = Category.objects.get(id=category_fk)
+        image_file = request.FILES.get('image_product')
+        price = request.POST['price']
+        product = Product(name=name, description=description, image_product=image_file, category=category, price=price)
+        product.save()
+        context = {'categories': categories,
+                   'product_added': True}
+        return render(request, 'new_product.html', context)
+    else:
+        context = {'categories': categories,
+                   'product_added': False}
+        return render(request, 'new_product.html', context)
 
 
 def contacts(request):
