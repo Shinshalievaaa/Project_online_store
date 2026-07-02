f0630f94473e08f8c5717c8f25d2a570d2ac7ed2
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2026-07-02 11:04:53 +0500

    fix

--- config/settings.py
+++ config/settings.py
@@ -108,7 +108,7 @@ EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'shinshila-a@yandex.ru' # Почта отправителя
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') # Почта отправителя
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
